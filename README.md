79eeb22d2501d22fe6d10dd3d68142dc98e02206
sharpecs <shaynejms@gmail.com>
2025-04-06 10:16:01 +0800

    init web project.

---
+++ README.md
@@ -0,0 +1,48 @@
+<!-- ABOUT THE PROJECT -->
+## Calyptus 
+
+<figure>
+    <center>
+    <img src="./assets/sample/screenshots.png">
+    <center>
+</figure>
+
+Calyptus is a nest box monitoring tool used to enrich the surveillance experience by providing feedback on how well an artificial hollow is installed. It was created to speed up time recording observational notes with a conservative approach for minimal distubances to wildlife. It is also used to determine Encounter Probability, Activity Rate and Occupancy Rate for comparison and sharing with wider community.
+
+Is anybody home?! A physical inspection can potentially disturb wildlife, so time spent should be kept at a minimum -- wildlife always comes first!
+
+The tool follows a three step approach:
+* A distant observation;
+* A close external examination; and
+* An internal inspection;
+
+Of course wildlife occurrences are opportunistic, the tool allows you to follow a standardised approach for creating a way for biodiversity conservation outcomes to by measured. A picture of wildlife movements associated with a habitat can easily be visualised.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+
+This project follows the
+[simple app state management
+tutorial](https://flutter.dev/docs/development/data-and-backend/state-mgmt/simple) as a starting 
+point for a Flutter application. The Flutter framework is used to leverage multi-platform
+capabilities.
+
+For help getting started with Flutter development, view the
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+* [![Flutter][Flutter.dev]][https://flutter.dev/]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+Inspirations were drawn from experiences at Mary Carroll Wetland. The park serves as a base point
+for personal observations.
+
+* [Friends of Mary Carroll Wetland](https://www.sercul.org.au/marycarroll/)
+* [Simon Cherriman](https://www.simoncherriman.com.au/)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ flutter_service_worker.js
@@ -0,0 +1,216 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.bin": "dc5cbdb30412d217d5f9383a37672ce4",
+"assets/AssetManifest.json": "899d3597a2101bd1d9851270cf99f603",
+"assets/assets/calyptus.json": "9c6be56df94a7d860ffe7dbf1b8dd81e",
+"assets/assets/icon/calyptus.png": "c25a515e782fd83e9f683cf584736683",
+"assets/assets/images/1676515489521492.png": "36941971c5567a34c6dd542b599fd618",
+"assets/assets/images/1676515489521493.png": "c760a4a4b2556767facbad872453f4c6",
+"assets/assets/images/1676515489521494.png": "36a2ecb7545611c38265c2dcbc6b17d5",
+"assets/assets/images/1676515489521495.png": "a81254a61c34fb9a9b7819a8cd8c0cb9",
+"assets/assets/images/1676515489521496.png": "9e29ca96463a7d8a867adb2c0df30339",
+"assets/assets/images/1676515489521500.png": "caa67b1e9d68279dabee86fb94cdc15d",
+"assets/assets/images/1676515489521505.png": "0f04a47a817241b07dcdb7a05727451c",
+"assets/assets/images/1676515489521507.png": "bb770fbd8316f03d071c20bb6ff251c2",
+"assets/assets/images/1696583212241368.png": "926144003ac88ff3485e25826ab26622",
+"assets/assets/images/2.0x/flutter_logo.png": "4efb9624185aff46ca4bf5ab96496736",
+"assets/assets/images/3.0x/flutter_logo.png": "b8ead818b15b6518ac627b53376b42f2",
+"assets/assets/images/676515489521497.png": "aae13e695d9ce02a5450730dfe75d71b",
+"assets/assets/images/flutter_logo.png": "478970b138ad955405d4efda115125bf",
+"assets/assets/images/map_mcw.png": "74339c19176aabbba28e0067f54ecb97",
+"assets/assets/images/sat_mcw.png": "00919dbe8ce7aa81114e523591d0814b",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/fonts/MaterialIcons-Regular.otf": "4c349cc5d142c290ad6804c6db0c8dfd",
+"assets/NOTICES": "121afa682cd68a93915cfc8e55b78b3f",
+"assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
+"canvaskit/canvaskit.wasm": "42df12e09ecc0d5a4a34a69d7ee44314",
+"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee22aebb0",
+"canvaskit/chromium/canvaskit.wasm": "be0e3b33510f5b7b0cc76cc4d3e50048",
+"canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
+"canvaskit/skwasm.wasm": "1a074e8452fe5e0d02b112e22cdcf455",
+"canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15",
+"favicon.png": "cbcb5dcd79b2bb4b28ceabc76973569c",
+"flutter.js": "6b515e434cea20006b3ef1726d2c8894",
+"icons/Icon-192.png": "9541f557851769bc57949eff4be1a006",
+"icons/Icon-512.png": "4dd90a6a2b1da94cf3a6a8d97bab8d1a",
+"icons/Icon-maskable-192.png": "9541f557851769bc57949eff4be1a006",
+"icons/Icon-maskable-512.png": "4dd90a6a2b1da94cf3a6a8d97bab8d1a",
+"index.html": "4b14ccfcdad0a4a6a7a711a6f7b74e92",
+"/": "4b14ccfcdad0a4a6a7a711a6f7b74e92",
+"main.dart.js": "8fefbdcf330fec438d8218de3299ae02",
+"manifest.json": "8869c2daac57ac7fafd766463ad7eec6",
+"version.json": "95cafcbd80e91fcc4277973e62e30b28"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
